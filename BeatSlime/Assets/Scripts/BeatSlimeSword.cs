de02133362c3280f394cd64e5c03708adc8d7466
PacosLelouch <276613158@qq.com>
2023-04-17 15:19:27 -0400

    Add tags. Initialize some scripts.

---
+++ BeatSlime/Assets/Scripts/BeatSlimeSword.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatSlimeSword : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region Collision & trigger.
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+    #endregion
+}
