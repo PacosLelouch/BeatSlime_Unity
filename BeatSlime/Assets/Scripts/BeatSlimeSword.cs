52c5fd00db67d3028ec4c3729c9f360c3bccd7a7
PacosLelouch <276613158@qq.com>
2023-04-19 18:08:06 -0400

    Update sword. Update player.

--- BeatSlime/Assets/Scripts/BeatSlimeSword.cs
+++ BeatSlime/Assets/Scripts/BeatSlimeSword.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
+//-------------------------------------------------------------------------
+[RequireComponent(typeof(Interactable))]
 public class BeatSlimeSword : MonoBehaviour
 {
+    private Vector3 oldPosition;
+    private Quaternion oldRotation;
+
+    private float attachTime;
+
+    private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
+
+    private Interactable interactable;
+    private bool lastHovering = false;
+
+    //-------------------------------------------------
+    void Awake()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +32,48 @@ public class BeatSlimeSword : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (interactable.isHovering != lastHovering) //save on the .tostrings a bit
+        {
+            lastHovering = interactable.isHovering;
+        }
     }
+
+    #region Interactable.
+    //-------------------------------------------------
+    // Called every Update() while a Hand is hovering over this object
+    //-------------------------------------------------
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(this.gameObject);
+
+        if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
+        {
+            // Save our position/rotation so that we can restore it when we detach
+            oldPosition = transform.position;
+            oldRotation = transform.rotation;
+
+            // Call this to continue receiving HandHoverUpdate messages,
+            // and prevent the hand from hovering over anything else
+            hand.HoverLock(interactable);
+
+            // Attach this object to the hand
+            hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
+        }
+        else if (isGrabEnding)
+        {
+            // Detach this object from the hand
+            hand.DetachObject(gameObject);
+
+            // Call this to undo HoverLock
+            hand.HoverUnlock(interactable);
+
+            // Restore position/rotation
+            //transform.position = oldPosition;
+            //transform.rotation = oldRotation;
+        }
+    }
+    #endregion
 
     #region Collision & trigger.
     private void OnCollisionEnter(Collision collision)
